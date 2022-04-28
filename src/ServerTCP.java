45d023d869e4e2657493ff8abca5d6274618dfb1
Damian Kaminski <arthuradd@yahoo.com>
2022-04-28 15:10:17 +0200

    Implemented a server that sends the input back to the client

--- src/ServerTCP.java
+++ src/ServerTCP.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -23,18 +28,33 @@ public class ServerTCP {
         while(true) {
             try {
                 client = server.accept();
+                resend();
+                client.close();
             } catch (IOException ex) {
                 System.err.println("Problem with accepting a connection");
                 ex.printStackTrace();
             }
+        }
+    }
 
-            try {
-                new PrintStream(client.getOutputStream()).println("Odebrano połączenie");
-                client.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
+    private void resend() {
+        BufferedWriter writer;
+        BufferedReader reader;
+        String line;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+                writer.flush();
+                System.out.println(line);
             }
 
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
