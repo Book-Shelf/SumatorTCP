2c72a04b3393155f4ce8f140fa110913b5fed81c
Damian Kaminski <arthuradd@yahoo.com>
2022-04-28 14:30:02 +0200

    implemented simple server
    It establishes a connection sends message and closes the connection

--- src/ServerTCP.java
+++ src/ServerTCP.java
@@ -1,3 +1,40 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerTCP {
+
+    ServerSocket server;
+    Socket client;
     
+    ServerTCP() {
+        try {
+            server = new ServerSocket(2020);
+        } catch (IOException ex) {
+            System.err.println("Problem with opening server socket");
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void listen() {
+
+        while(true) {
+            try {
+                client = server.accept();
+            } catch (IOException ex) {
+                System.err.println("Problem with accepting a connection");
+                ex.printStackTrace();
+            }
+
+            try {
+                new PrintStream(client.getOutputStream()).println("Odebrano połączenie");
+                client.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
 }
